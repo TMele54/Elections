f3d12e0d8e6144d8010d6738a28f101d27ef64b3
Anthony Mele <TonyMele.Rutgers@Gmail.com>
2020-11-12 03:41:43 -0500

    switch year

--- static/js/map.js
+++ static/js/map.js
@@ -1,94 +1,103 @@
-//Width and height of map
-var width = 960;
-var height = 500;
 
-// D3 Projection
-var projection = d3.geo.albersUsa().translate([width/2, height/2]).scale([1000]);
+function draw_map(YEAR){
 
-// Define path generator
-var path = d3.geo.path().projection(projection);
+    //Width and height of map
+    var width = 960;
+    var height = 500;
 
-// Define linear scale for output
-var color = d3.scale.linear().range(["rgb(255,0,0)","rgb(0,0,255)"]);
-var legendText = ["Democratic","Republican"];
+    // D3 Projection
+    var projection = d3.geo.albersUsa().translate([width/2, height/2]).scale([1000]);
 
-//Create SVG element and append map to the SVG
-var svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
+    // Define path generator
+    var path = d3.geo.path().projection(projection);
 
-var div = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
+    // Define linear scale for output
+    var color = d3.scale.linear().range(["rgb(255,0,0)","rgb(0,0,255)"]);
+    var legendText = ["Democratic","Republican"];
 
-// Load in my states data!
-d3.json("../static/data/vizdata/vizbaby.json", function(data) {
-    color.domain([0,1]);
-    data = data['2012']
-    // Load GeoJSON data and merge with states data
-    d3.json("../static/geo/us-states.geojson", function(json) {
+    //Create SVG element and append map to the SVG
+    var svg = d3.select("#mapp").append("svg").attr("width", width).attr("height", height);
 
-        // Loop through each state data value in the .csv file
-        for (var i = 0; i < data.length; i++) {
+    var div = d3.select("#mapp").append("div").attr("class", "tooltip").style("opacity", 0);
 
-            // Grab State Name
-            var dataState = data[i].state;
+    // Load in my states data!
+    d3.json("../static/data/vizdata/vizbaby.json", function(data) {
+        color.domain([0,1]);
 
-            // Grab data value
-            var dataValue = data[i].state_majority;
+        var data = data[YEAR]
 
-            // Find the corresponding state inside the GeoJSON
-            for (var j = 0; j < json.features.length; j++)  {
-                var jsonState = json.features[j].properties.name;
+        // Load GeoJSON data and merge with states data
+        d3.json("../static/geo/us-states.geojson", function(json) {
 
-                if (dataState == jsonState) {
+            // Loop through each state data value in the .csv file
+            for (var i = 0; i < data.length; i++) {
 
-                    // Copy the data value into the JSON
-                    json.features[j].properties.state_majority = dataValue;
+                // Grab State Name
+                var dataState = data[i].state;
 
-                // Stop looking through the JSON
-                break;
+                // Grab data value
+                var dataValue = data[i].state_majority;
+
+                // Find the corresponding state inside the GeoJSON
+                for (var j = 0; j < json.features.length; j++)  {
+                    var jsonState = json.features[j].properties.name;
+
+                    if (dataState == jsonState) {
+
+                        // Copy the data value into the JSON
+                        json.features[j].properties.state_majority = dataValue;
+
+                    // Stop looking through the JSON
+                    break;
+                    }
                 }
             }
-        }
 
-        // Bind the data to the SVG and create one path per GeoJSON feature
-        svg.selectAll("path")
-            .data(json.features)
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .style("stroke", "#fff")
-            .style("stroke-width", "1")
-            .style("fill", function(d) {
-                var value = d.properties.state_majority;
-                if (value == "Republican") {
-                    var val = 0
-                }else{
-                    var val = 1
-                }
-                return color(val)
-        });
+            // Bind the data to the SVG and create one path per GeoJSON feature
+            svg.selectAll("path")
+                .data(json.features)
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .style("stroke", "#fff")
+                .style("stroke-width", "1")
+                .style("fill", function(d) {
+                    var value = d.properties.state_majority;
+                    if (value == "Republican") {
+                        var val = 0
+                    }else{
+                        var val = 1
+                    }
+                    return color(val)
+            });
 
 
-    // Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
-    var legend = d3.select("body").append("svg")
-                    .attr("class", "legend")
-                    .attr("width", 140)
-                    .attr("height", 200)
-                    .selectAll("g")
-                    .data(color.domain().slice().reverse())
-                    .enter()
-                    .append("g")
-                    .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+        // Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
+        var legend = d3.select("#mapp").append("svg")
+                        .attr("class", "legend")
+                        .attr("width", 140)
+                        .attr("height", 200)
+                        .selectAll("g")
+                        .data(color.domain().slice().reverse())
+                        .enter()
+                        .append("g")
+                        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-        legend.append("rect")
-              .attr("width", 18)
-              .attr("height", 18)
-              .style("fill", color);
+            legend.append("rect")
+                  .attr("width", 18)
+                  .attr("height", 18)
+                  .style("fill", color);
 
-        legend.append("text")
-              .data(legendText)
-              .attr("x", 24)
-              .attr("y", 9)
-              .attr("dy", ".35em")
-              .text(function(d) { return d; });
-        });
+            legend.append("text")
+                  .data(legendText)
+                  .attr("x", 24)
+                  .attr("y", 9)
+                  .attr("dy", ".35em")
+                  .text(function(d) { return d; });
+            });
 
-});
+    });
+
+}
+
+draw_map("1900")
